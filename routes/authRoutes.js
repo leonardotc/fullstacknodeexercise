e373a91020195ee0f59d3b31b85de70ad9643779
Leonardo Cardoso <leonardo.t.cardoso@hotmail.com>
2017-08-18 15:51:34 -0300

    master dirtyness

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,7 +4,6 @@ module.exports = (app) => {
     app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }))
     app.get('/auth/google/callback', passport.authenticate('google'))
     app.get('/auth/logout', (req, rsp) => {
-        console.log(req.user)
         req.logout()
         rsp.send(req.user)
     })
