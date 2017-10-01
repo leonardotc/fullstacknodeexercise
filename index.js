e8da6fd49098df6391648bee7b82ffafc9b54ae5
Leonardo Cardoso <leonardo.t.cardoso@hotmail.com>
2017-10-01 10:27:49 -0300

    Fix relative path for production

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ require('./routes/billingRoutes')(app)
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'))
     const path = require('path')
-    app.get('*', (req, rsp) => rsp.sendFile(path.resolve(__dirname, 'client','build','index.html')))
+    app.get('*', (req, rsp) => rsp.sendFile(path.resolve(__dirname, 'client/build','index.html')))
 }
 
 app.listen(process.env.PORT || 5000, () => console.log("listening..."))
